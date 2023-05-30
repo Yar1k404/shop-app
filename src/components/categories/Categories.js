b2f0fae35ff3ec7f1de576986106eb37f183a938
Yar1k404 <moloko0107@gmail.com>
2023-05-30 20:51:48 +0300

    added animation when switching categories

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 import './Categories.css'
 
 function Categories({chooseCategory}) {
+    const [selectedCategory, setSelectedCategory] = useState('all')
+
     const [categories, setCategories] = useState([
         {
             key: 'all',
@@ -25,10 +27,20 @@ function Categories({chooseCategory}) {
         }
     ])
 
+    const handleCategoryClick = (categoryKey) => {
+        setSelectedCategory(categoryKey)
+        chooseCategory(categoryKey)
+    }
+
     return (
         <div className='categories'>
             {categories.map(item => (
-                <div className='category' key={item.key} onClick={() => chooseCategory(item.key)}>{item.name}</div>
+                <div
+                    className={`category ${selectedCategory === item.key ? "selected" : ""}`}
+                    key={item.key}
+                    onClick={() => handleCategoryClick(item.key)}>
+                    {item.name}
+                </div>
             ))}
         </div>
     )
